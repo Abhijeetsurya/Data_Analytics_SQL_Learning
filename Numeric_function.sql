bed51868743cf8e4a6166cd62b4d44837bed00ce
Abhijeet Suryawanshi <87439962+Abhijeetsurya@users.noreply.github.com>
2025-04-26 13:33:43 +0530

    Add files via upload

---
+++ Numeric_function.sql
@@ -0,0 +1,46 @@
+# Numeric Function
+
+
+# Round, ceil, floor, abs, mod, log, power
+
+# Round
+SELECT ROUND(25.78) AS Rounded_num;
+
+# Ceil
+
+SELECT CEIL(25.36) AS ceil;
+
+SELECT CEIL(-25.36) AS ceil;
+
+# Floor
+
+SELECT FLOOR(22.23) AS floor_;
+
+SELECT FLOOR(-22.23) AS floor_;
+
+# Power
+
+SELECT POWER(3, 2) As Square_of_3;
+
+
+# ABS 
+
+SELECT ABS(-23);
+
+
+# LOG
+
+SELECT LOG(10);
+
+SELECT LOG2(10);
+
+
+
+
+
+
+
+
+
+
+
